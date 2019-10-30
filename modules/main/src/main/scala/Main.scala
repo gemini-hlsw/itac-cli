19b8351011d630b8bc10646da56a890a05e619a1
Rob Norris <rob_norris@mac.com>
2019-10-30 16:09:26 -0500

    wip on converting to itac

--- modules/main/src/main/scala/Main.scala
+++ modules/main/src/main/scala/Main.scala
@@ -1,24 +1,46 @@
 // Copyright (c) 2016-2019 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package edu.gemini.itac
+package itac
 
 import cats.effect._
 import cats.implicits._
 import com.monovore.decline.effect.CommandIOApp
 import com.monovore.decline.Opts
-import com.monovore.decline.Command
+import java.io.File
+import edu.gemini.tac.qengine.p1.io.JointIdGen
+import edu.gemini.tac.qengine.ctx.Partner
+import edu.gemini.tac.qengine.util.Percent
+import edu.gemini.tac.qengine.ctx.Site
+import edu.gemini.spModel.core.Semester
 
 object Main extends CommandIOApp(
   name = "itac",
   header = "ITAC Command Line Interface"
 ) {
 
-  val init = Command(name = "init", header = "Initialize an ITAC workspace.") { Opts.unit }
-  val ls   = Command(name = "ls", header = "List proposals in the current workspace.") { Opts.unit }
+  val partners = Map(
+    "US" -> Partner("US", "United States", Percent(0.5), Set(Site.north, Site.south)),
+    "CL" -> Partner("CL", "Chile", Percent(0.5), Set(Site.north, Site.south)),
+    "AR" -> Partner("AR", "Argentina", Percent(0.5), Set(Site.north, Site.south)),
+  )
+
+  val when = new Semester(2019, Semester.Half.B).getMidpointDate(edu.gemini.spModel.core.Site.GN).getTime()
 
   def main: Opts[IO[ExitCode]] =
-    (Opts.subcommand(init) |+| Opts.subcommand(ls)
-    ).map(s => IO(println(s)).as(ExitCode.Success))
+    Opts.unit.map { _ =>
 
-}
+      ProposalLoader[IO](partners, when)
+        .load(new File("/Users/rnorris/proposals-xml/US_2019B_156.xml"))
+        .runA(JointIdGen(1))
+        .flatMap { //p =>
+          // ps.traverse_ {
+            case Left(errs) => IO(println("---")) *> errs.traverse(e => IO(println(e)))
+            case Right(ps)  => ps.traverse(p => IO(println(p)))
+          // }
+        }
+        .as(ExitCode.Success)
+
+    }
+
+  }
--- modules/main/src/main/scala/ProposalLoader.scala
+++ modules/main/src/main/scala/ProposalLoader.scala
@@ -1,16 +1,30 @@
 // Copyright (c) 2016-2019 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package edu.gemini.itac
+package itac
 
-import cats.Parallel
+import cats._
+import cats.data._
 import cats.effect.Sync
 import cats.implicits._
-import edu.gemini.model.p1.{mutable => M}
-import edu.gemini.model.p1.immutable.Proposal
+import edu.gemini.model.p1.{mutable => M, immutable => I}
 import java.io.File
 import java.lang.reflect.Constructor
 import javax.xml.bind.JAXBContext
+import edu.gemini.tac.qengine.p1.Proposal
+import edu.gemini.tac.qengine.p1.io.ProposalIo
+import edu.gemini.tac.qengine.p1.io.JointIdGen
+import edu.gemini.tac.qengine.ctx.Partner
+
+trait ProposalLoader[F[_]] {
+
+  def load(file: File): StateT[F, JointIdGen, EitherNel[String, NonEmptyList[Proposal]]]
+
+  // problem here is all the errors are smashed together
+
+  def loadMany(file: File): StateT[F, JointIdGen, List[EitherNel[String, NonEmptyList[Proposal]]]]
+
+}
 
 object ProposalLoader {
 
@@ -24,20 +38,48 @@ object ProposalLoader {
     JAXBContext.newInstance(contextPackage, getClass.getClassLoader)
   }
 
-  private val ctor: Constructor[Proposal] =
-    classOf[Proposal].getConstructor(classOf[M.Proposal])
+  private val ctor: Constructor[I.Proposal] =
+    classOf[I.Proposal].getConstructor(classOf[M.Proposal])
 
-  private def unsafeLoad(file: File): Proposal =
-    ctor.newInstance(context.createUnmarshaller.unmarshal(file))
+  def apply[F[_]: Sync: Parallel](
+    partners: Map[String, Partner],
+    when: Long
+  ): ProposalLoader[F] =
+    new ProposalLoader[F] {
 
-  def load[F[_]: Sync](file: File): F[Proposal] =
-    Sync[F].delay(unsafeLoad(file))
+      private def unsafeLoadPhase1(file: File): I.Proposal =
+        ctor.newInstance(context.createUnmarshaller.unmarshal(file))
 
-  def loadMany[F[_]: Sync: Parallel](dir: File): F[List[Proposal]] =
-    Sync[F].delay(Option(dir.listFiles)).flatMap {
-      case Some(arr) => arr.toList.parTraverse(load(_))
-      case None      => Sync[F].raiseError(new RuntimeException(s"Not a directory: $dir"))
+      def loadPhase1(file: File): F[I.Proposal] =
+        Sync[F].delay(unsafeLoadPhase1(file))
+
+      def loadManyPhase1(dir: File): F[List[I.Proposal]] =
+        Sync[F].delay(Option(dir.listFiles)).flatMap {
+          case Some(arr) => arr.toList.parTraverse(loadPhase1(_))
+          case None      => Sync[F].raiseError(new RuntimeException(s"Not a directory: $dir"))
+        }
+
+      val pio: ProposalIo =
+        new ProposalIo(partners)
+
+      def read(proposal: I.Proposal): State[JointIdGen, EitherNel[String, NonEmptyList[Proposal]]] =
+        State { jig =>
+          pio.read(proposal, when, jig) match {
+            case scalaz.Failure(ss)         => (jig,  NonEmptyList(ss.head, ss.tail.toList).asLeft)
+            case scalaz.Success((ps, jigʹ)) => (jigʹ, NonEmptyList(ps.head, ps.tail.toList).asRight)
+          }
+        }
+
+      def load(file: File): StateT[F, JointIdGen, EitherNel[String, NonEmptyList[Proposal]]] =
+        StateT { jig => loadPhase1(file).map(read(_).run(jig).value) }
+
+      def loadMany(dir: File): StateT[F, JointIdGen, List[EitherNel[String, NonEmptyList[Proposal]]]] =
+        StateT { jig => loadManyPhase1(dir).map(_.traverse(read(_)).run(jig).value) }
+
     }
 
 }
+
+
+
 
