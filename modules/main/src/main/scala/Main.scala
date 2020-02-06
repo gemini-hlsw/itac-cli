b2fdfe3afc11f68b6553d44fb7d1cea9f4d49a43
Rob Norris <rob_norris@mac.com>
2020-02-06 10:12:11 -0600

    improve help text

--- modules/main/src/main/scala/Main.scala
+++ modules/main/src/main/scala/Main.scala
@@ -24,7 +24,35 @@ import org.slf4j.impl.ColoredSimpleLogger
 
 object Main extends CommandIOApp(
   name    = "itac",
-  header  = "ITAC Command Line Interface"
+  header  =
+    s"""|For more information pass `--help` to a subcommand, for instance `itac init --help`.
+        |
+        |ITAC Command Line Interface
+        |
+        |This is a simple program that generates queues by reading proposal XML files and a set of
+        |configuration files that you (the user) edit to fine-tune the queue to your liking. There
+        |is no database. The workspace contains all the information about queue generation. There
+        |are no external dependencies.
+        |
+        |The general workflow is:
+        |
+        | - use `itac init` to create a new workspace
+        | - copy proposal XML files into the proposals/ directory in your workspace
+        | - edit the common.yaml file as necessary (it contains instructions)
+        | - edit the site-specific configuration files as necessary (they contain instructions)
+        | - use `itac queue` to generate a queue
+        | - refine your configurations (and/or create new ones) until you're happy with the queue
+        | - export program skeletons to the observing database with `itac skeleton`
+        | - edit email templates as necessary
+        | - notify PIs with `itac email`
+        |
+        |Note that itac will never change proposal XML files, nor will it override any existing file
+        |unless you specify the `--force` option. It is ok to experiment, you're not going to cause
+        |any damage by doing so.
+        |
+        |The options below are valid for all sub-commands. For information on specific sub-commands
+        |try `itac <sub-command> --help`, for example `itac rollover --help`.
+        |""".stripMargin.trim
 ) with MainOpts {
 
   def main: Opts[IO[ExitCode]] =
@@ -51,7 +79,7 @@ trait MainOpts { this: CommandIOApp =>
     Opts.option[Path](
       short = "d",
       long  = "dir",
-      help  = "Working directory. Default is current directory."
+      help  = "Workspace directory. Default is current directory."
     ) .withDefault(Paths.get(System.getProperty("user.dir")))
 
   lazy val commonConfig: Opts[Path] =
@@ -68,14 +96,14 @@ trait MainOpts { this: CommandIOApp =>
     } <+> Opts.option[Path](
       short = "-c",
       long  = "config",
-      help  = s"Site configuration file. Default is <site>-queue.yaml"
+      help  = s"Site configuration file, relative to workspace (or absolute). Default is <site>-queue.yaml"
     )
 
   lazy val rolloverReport: Opts[Option[Path]] =
     Opts.option[Path](
       short = "-r",
       long  = "rollover",
-      help  = s"Rollover report. Default is <site>-rollovers.yaml"
+      help  = s"Rollover report, relative to workspace (or absolute). Default is <site>-rollovers.yaml"
     ).orNone
 
   def out(default: Path): Opts[Path] =
@@ -98,7 +126,7 @@ trait MainOpts { this: CommandIOApp =>
       long    = "verbose",
       short   = "v",
       metavar = "level",
-      help    = "Log verbosity. One of: trace debug info warn error off. Defaults to info."
+      help    = "Log verbosity. One of: trace debug info warn error off. Default is info."
     ) .withDefault("info")
       .mapValidated {
         case s @ ("trace" | "debug" | "info" | "warn" | "error" | "off") =>
@@ -113,7 +141,10 @@ trait MainOpts { this: CommandIOApp =>
   lazy val init: Command[Operation[IO]] =
     Command(
       name   = "init",
-      header = "Initialize an ITAC workspace."
+      header =
+        s"""|Initialize an ITAC workspace in the current directory (override with --dir). Requires
+            |an internal network connection.
+            |""".stripMargin.trim
     )(semester.map(Init[IO]))
 
   lazy val ls: Command[Operation[IO]] =
@@ -144,7 +175,7 @@ trait MainOpts { this: CommandIOApp =>
     Opts.flag(
       long  = "force",
       short = "f",
-      help  = "Overwrite existing files."
+      help  = "Overwrite existing files (normally itac will not do this)."
     ).orFalse
 
   lazy val site = gn <+> gs
@@ -152,14 +183,35 @@ trait MainOpts { this: CommandIOApp =>
   lazy val rollover: Command[Operation[IO]] =
     Command(
       name   = "rollover",
-      header = "Generate a rollover report by fetching information from the observing database."
+      header =
+        s"""|Generate a rollover report by fetching information from the specified observing
+            |database. Requires an internal network connection.
+            |""".stripMargin.trim
     )((site, Opts.option[Path](
       short = "o",
       long  = "out",
       help  = s"Output file. Default is GN-rollover.yaml or GS-rollover.yaml"
     ).orNone).mapN(Rollover(_, _)))
 
+  object Placeholder extends Operation[IO] {
+    def run(ws: Workspace[IO], log: Logger[IO], b: Blocker): IO[ExitCode] =
+      log.error("This feature is not yet implemented").as(ExitCode.Error)
+  }
+
+  def placeholder(name: String): Command[Operation[IO]] =
+    Command(
+      name = name,
+      header = s"Placeholder for $name, which is not yet implemented."
+    )(Placeholder.pure[Opts])
+
   lazy val ops: Opts[Operation[IO]] =
-    List(init, ls, queue, rollover).sortBy(_.name).map(Opts.subcommand(_)).foldK
+    List(
+      placeholder("email"),
+      placeholder("skeleton"),
+      init,
+      ls,
+      rollover,
+      queue
+    ).sortBy(_.name).map(Opts.subcommand(_)).foldK
 
 }
